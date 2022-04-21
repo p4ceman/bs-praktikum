e37bfaeb5ad2dd37a42878204446442902326cb4
Benedikt Isken <bisken@gmx.de>
2022-04-21 18:26:06 +0200

    put function impletieren

--- keyValStore.c
+++ keyValStore.c
@@ -1,16 +1,52 @@
-//
-// Created by Benedikt Isken on 21.04.22.
-//
 
 #include "keyValStore.h"
+#include <string.h>
 
+#define ARRAYLENGTH 100
 
-int put(char* key, char* value) {
+typedef struct keyValue{
+    char* key;
+    char* value;
+} KeyValue;
+
+KeyValue dictionary[ARRAYLENGTH];
+
+void initializeDictionary() {
 
 }
+
+//checked Sonderzeichen und Leerzeichen
+int accept(char* string);
+
+//Die put() Funktion soll eine Wert (value) mit dem Schlüsselwert (key) hinterlegen.
+//Wenn der Schlüssel bereits vorhanden ist, soll der Wert überschrieben werden.
+//Der Rückgabewert der Funktion könnte Auskunft dazu geben.
+//Gibt 0 zurück wenn neuer Key hinzugefügt wurde, 1 wenn Value zu bereits existierendem Key überschrieben würde
+//und -1 wenn das Array voll ist.
+int put(char* key, char* value) {
+    //accept(key)
+    //accept(value)
+    int i = 0;
+    while (dictionary[i].key != NULL) {
+        if (strcmp(dictionary[i].key,key)==0) {
+            dictionary[i].value = value;
+            return(1);
+        }
+        i++;
+        if (i == 100) {
+            return(-1);
+        }
+    }
+    dictionary[i].key = key;
+    dictionary[i].value = value;
+    return(0);
+}
+
+
 int get(char* key, char* res) {
 
 }
+
 int del(char* key) {
 
 }
--- main.h
+++ main.h
@@ -1,6 +1,4 @@
-//
-// Created by Benedikt Isken on 21.04.22.
-//
+
 
 #ifndef BS_PRAKTIKUM_MAIN_H
 #define BS_PRAKTIKUM_MAIN_H
