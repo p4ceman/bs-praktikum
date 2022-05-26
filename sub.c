8227a5178bf3a646631f7c6794f9e55538847e88
p4ceman <74560465+p4ceman@users.noreply.github.com>
2022-05-26 12:09:16 +0200

    Added BEG and END to valid commands

--- sub.c
+++ sub.c
@@ -35,7 +35,7 @@ Rückgabewert:   1 => String ist valide
                 0 => String ist nicht valide*/
 int isInputValid(char* input) {
     regex_t regex;
-    regcomp(&regex,"^((((put|PUT) ([A-Za-z0-9])+|(get|GET)|(del|DEL)) ([A-Za-z0-9])+)|(quit|QUIT))$", REG_EXTENDED);
+    regcomp(&regex,"^((((put|PUT) ([A-Za-z0-9])+|(get|GET)|(del|DEL)) ([A-Za-z0-9])+)|((quit|QUIT)|(BEG|beg)|(END|end))$", REG_EXTENDED);
     if(regexec(&regex, input, 0, NULL, 0) == 0){
         return 1;
     } else{
